c6efb65e59bad87fb6cf9cc50c488937c9766285
Omar_Ameer <omar.ameer244@gmail.com>
2023-02-24 01:14:26 +0200

    Final Part_3 Commit

--- lib/screens/chat_scree.dart
+++ lib/screens/chat_scree.dart
@@ -1,5 +1,6 @@
 import 'package:chatgpt_app/constants/constants.dart';
 import 'package:chatgpt_app/services/assets_manager.dart';
+import 'package:chatgpt_app/widgets/chat_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -49,7 +50,11 @@ class _ChatScreenState extends State<ChatScreen> {
               child: ListView.builder(
                 itemCount: 6,
                 itemBuilder: (context, index) {
-                  return const Text("Hello, this is text!");
+                  return ChatWidget(
+                    msg: chatMessages[index]["msg"].toString(),
+                    chatIndex:
+                        int.parse(chatMessages[index]["chatIndex"].toString()),
+                  );
                 },
               ),
             ),
