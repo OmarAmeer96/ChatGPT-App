44c447953f3c3b846cc99d64fb5a7e82159c0c9e
Omar_Ameer <omar.ameer244@gmail.com>
2023-02-23 20:59:54 +0200

    Final Part_1 Commit

---
+++ lib/screens/chat_scree.dart
@@ -0,0 +1,20 @@
+import 'package:chatgpt_app/services/assets_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset(AssetsManager.openaiLogo),),
+    );
+  }
+}
