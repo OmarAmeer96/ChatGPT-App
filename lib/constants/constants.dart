94032c72dd434d0649953659543e2bdd5a394688
Omar_Ameer <omar.ameer244@gmail.com>
2023-02-25 00:43:16 +0200

    Start Displaying all Models Using FutureBuilder

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,27 +4,27 @@ import 'package:flutter/material.dart';
 Color scaffoldBackgroundColor = const Color(0xFF343541);
 Color cardColor = const Color(0xFF444654);
 
-List<String> models = [
-  'Model 1',
-  'Model 2',
-  'Model 3',
-  'Model 4',
-  'Model 5',
-  'Model 6',
-];
+// List<String> models = [
+//   'Model 1',
+//   'Model 2',
+//   'Model 3',
+//   'Model 4',
+//   'Model 5',
+//   'Model 6',
+// ];
 
-List<DropdownMenuItem<String?>>? get getModelsItem {
-  List<DropdownMenuItem<String?>>? modelsItems =
-      List<DropdownMenuItem<String?>>.generate(
-          models.length,
-          (index) => DropdownMenuItem(
-              value: models[index],
-              child: TextWidget(
-                label: models[index],
-                fontSize: 15,
-              )));
-  return modelsItems;
-}
+// List<DropdownMenuItem<String?>>? get getModelsItem {
+//   List<DropdownMenuItem<String?>>? modelsItems =
+//       List<DropdownMenuItem<String?>>.generate(
+//           models.length,
+//           (index) => DropdownMenuItem(
+//               value: models[index],
+//               child: TextWidget(
+//                 label: models[index],
+//                 fontSize: 15,
+//               )));
+//   return modelsItems;
+// }
 
 final chatMessages = [
   {
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,3 +1,6 @@
+import 'package:chatgpt_app/models/models_model.dart';
+import 'package:chatgpt_app/services/api_service.dart';
+import 'package:chatgpt_app/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/constants.dart';
@@ -10,18 +13,40 @@ class ModelsDropDownWidget extends StatefulWidget {
 }
 
 class _ModelsDropDownWidgetState extends State<ModelsDropDownWidget> {
-  String currentModel = "Model 1";
+  String currentModel = "text-davinci-003";
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      dropdownColor: scaffoldBackgroundColor,
-      iconEnabledColor: Colors.white,
-      items: getModelsItem,
-      value: currentModel,
-      onChanged: (value) {
-        setState(() {
-          currentModel = value.toString();
-        });
+    return FutureBuilder <List<ModelsModel>>(
+      future: ApiService.getModels(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: TextWidget(label: snapshot.error.toString()),
+          );
+        }
+        return snapshot.data == null || snapshot.data!.isEmpty
+            ? const SizedBox.shrink()
+            : FittedBox(
+              child: DropdownButton(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: 
+                  List<DropdownMenuItem<String?>>.generate(
+                  snapshot.data!.length,
+                  (index) => DropdownMenuItem(
+                      value: snapshot.data![index].id,
+                      child: TextWidget(
+                        label: snapshot.data![index].id,
+                        fontSize: 15,
+                      ))),
+                  value: currentModel,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModel = value.toString();
+                    });
+                  },
+                ),
+            );
       },
     );
   }
