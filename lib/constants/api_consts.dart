19b6ddd04309ba942831a3364c1aa341b92a7988
Omar_Ameer <omar.ameer244@gmail.com>
2023-03-04 01:42:10 +0200

    Change App Icon

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
